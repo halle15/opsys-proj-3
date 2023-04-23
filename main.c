2b2e3e36b1bc19a1ce1e73a302435b44371b41ae
Evann Hall <halle15@email.franklin.edu>
2023-04-23 01:00:46 -0400

    starting exploration, helper functions

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+// compile: gcc -o proj3 main.c
+
+void print_binary(unsigned int num) {
+    for (int i = 31; i >= 0; i--) {
+        printf("%d", (num >> i) & 1);
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    printf("init");
+    int s = 1;
+    printf("size: %d", sizeof(s));
+    for(int i = 0; i < 1000; i++){
+        print_binary(i);
+        printf("\n");
+        usleep(100000);
+    }
     return 0;
 }
