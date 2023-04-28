5546b8674175803a072d06e3d2f520098295b9cc
Evann Hall <halle15@email.franklin.edu>
2023-04-27 23:26:26 -0400

    better problem solving in binary.c helper file

--- learning files/binary.c
+++ learning files/binary.c
@@ -1,70 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// compile command: gcc -o binary -O3 binary.c
+#define PAGE_SIZE 256
+#define FRAME_SIZE 256
 
-int info_extract(int num){
-    return (num >> 16) & 0xFFFF;
-}
+#define NUMBER_OF_FRAMES 256
+#define PAGE_TABLE 256
+#define TLB_SIZE 16
 
-int page_extract(int num){
-    return (num >> 8) & 0xFF;
-}
-
-int offset_extract(int num){
-    return num & 0xFF;
-}
-
-// learning function, bunch of printers
-int bit_mask()
+int main()
 {
-    int number = 0xFF00;
-    int mask;
+    unsigned int logical_address;
+    unsigned int page_number;
+    unsigned int offset;
 
-    // AND operation (masking only the lower 8 bits)
-    mask = 0x000000FF; // 32-bit mask: 0000 0000 0000 0000 0000 0000 1111 1111
-    int and_result = (number >> 8) & mask;
-
-    // OR operation (setting the upper 8 bits to 1)
-    mask = 0xFF000000; // 32-bit mask: 1111 1111 0000 0000 0000 0000 0000 0000
-    int or_result = number | mask;
-
-    // XOR operation (inverting the middle 16 bits)
-    mask = 0x00FFFF00; // 32-bit mask: 0000 0000 1111 1111 1111 1111 0000 0000
-    int xor_result = number ^ mask;
-
-    printf("Original number: %d = 0x%08X\n", number, number);
-    printf("AND result: %d = 0x%08X\n", and_result, and_result);
-    printf("OR result: %d = 0x%08X\n", or_result, or_result);
-    printf("XOR result: %d = 0x%08X\n", xor_result, xor_result);
-}
-
-void print_binary(unsigned int num)
-{
-    for (int i = 31; i >= 0; i--)
+    // Open the input file with the logical addresses
+    FILE *input_file = fopen("addresses.txt", "r");
+    if (input_file == NULL)
     {
-        printf("%d", (num >> i) & 1);
+        printf("Error: Could not open addresses.txt\n");
+        exit(1);
     }
-}
 
-/* First, write a simple program that extracts the page number and offset based on: | 31-16 INFORMATION | 15-8 PAGE NUMBER | 7-0 OFFSET |
- */
-
-int main(int argc, char const *argv[])
-{
-    unsigned int s = atoi(argv[1]);
-    // printf("size: %d\n", sizeof(s));
-    // bit_mask();
-    /*
-    for (int i = 0; i < 1000; i++)
+    // Open the backing store
+    FILE *backing_store = fopen("BACKING_STORE.bin", "rb");
+    if (backing_store == NULL)
     {
-        print_binary(i);
-        printf(" - %d\n", i);
-        usleep(100000);
+        printf("Error: Could not open BACKING_STORE.bin\n");
+        exit(1);
     }
-    */
-    printf("Num: %d\n===================\n", s);
-    printf("Offset: %d\n", offset_extract(s));
-    printf("Page Number: %d\n", page_extract(s));
-    printf("Info: %d\n", info_extract(s));
+
+    // Read the logical addresses from the input file
+    while (fscanf(input_file, "%u", &logical_address) != EOF)
+    {
+    page_number = (logical_address & 0xFFFF) >> 8;
+    offset = logical_address & 0xFF;
+
+    // Calculate the physical address
+    unsigned int physical_address = (page_number * FRAME_SIZE) + offset;
+
+    // Read the value at the physical address from the backing store
+    char value;
+    fseek(backing_store, physical_address, SEEK_SET);
+    fread(&value, sizeof(char), 1, backing_store);
+
+    printf("Logical address: %u, Physical address: %u, Value: %d\n", logical_address, physical_address, value);
+
+    // Close the backing store file
+    }
+
+    // Close the files
+    fclose(input_file);
+    fclose(backing_store);
+
     return 0;
 }
